104aba32cf58321c28d5d9d0c120a776a963d836
Nate Abele <nate.abele@gmail.com>
2013-09-12 21:39:31 -0400

    Refactoring $view.load() to always return a promise.  - Reimplemented as a service for less boilerplate.  - Added documentation.

--- src/view.js
+++ src/view.js
@@ -1,8 +1,8 @@
-
-$ViewProvider.$inject = [];
-function $ViewProvider() {
+$View.$inject = ['$rootScope', '$templateFactory', '$q', '$rootScope'];
+function $View(   $rootScope,   $templateFactory,   $q,   $rootScope) {
 
   this.$get = $get;
+
   /**
    * @ngdoc object
    * @name ui.router.state.$view
@@ -15,42 +15,46 @@ function $ViewProvider() {
    */
   $get.$inject = ['$rootScope', '$templateFactory'];
   function $get(   $rootScope,   $templateFactory) {
-    return {
-      // $view.load('full.viewName', { template: ..., controller: ..., resolve: ..., async: false, params: ... })
-      /**
-       * @ngdoc function
-       * @name ui.router.state.$view#load
-       * @methodOf ui.router.state.$view
-       *
-       * @description
-       *
-       * @param {string} name name
-       * @param {object} options option object.
-       */
-      load: function load(name, options) {
-        var result, defaults = {
-          template: undefined,
-          templateUrl: undefined,
-          templateProvider: undefined,
-          controller: null,
-          locals: null,
-          notify: true,
-          async: true,
-          params: {}
-        };
-        options = extend(defaults, options);
 
-        if (options.template || options.templateUrl || options.templateProvider) {
-          result = $templateFactory.fromConfig(options, options.params, options.locals);
-        }
-        if (result && options.notify) {
-          options.targetView = name;
-          $rootScope.$broadcast('$viewContentLoading', options);
-        }
-        return result;
+    // $view.load('full.viewName', { template: ..., controller: ..., resolve: ..., async: false, params: ... })
+    /**
+     * @ngdoc function
+     * @name ui.router.state.$view#load
+     * @methodOf ui.router.state.$view
+     *
+     * @description
+     *
+     * @param {string} name name
+     * @param {object} options option object.
+     */
+    this.load = function load (name, options) {
+      var result, defaults = {
+        template: undefined,
+        templateUrl: undefined,
+        templateProvider: undefined,
+        controller: null,
+        locals: null,
+        notify: true,
+        async: true,
+        params: {}
+      };
+      options = extend(defaults, options);
+
+      if (options.template || options.templateUrl || options.templateProvider) {
+        result = $templateFactory.fromConfig(options, options.params, options.locals);
       }
+      if (result && options.notify) {
+        options.targetView = name;
+        $rootScope.$broadcast('$viewContentLoading', options);
+      }
+      if (isString(result)) {
+        var deferred = $q.defer();
+        $rootScope.$evalAsync(function() { deferred.resolve(result); });
+        return deferred.promise;
+      }
+      return result;
     };
   }
 }
 
-angular.module('ui.router.state').provider('$view', $ViewProvider);
+angular.module('ui.router.state').service('$view', $View);
